0e4d45d0eb1e68dc96c8cd677bb8242ce37ba44f
Chiwan Park <chiwanpark@icloud.com>
2013-07-15 12:36:20 +0900

    add database setting for test.

---
+++ anbadoserver/config.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+DATABASE_URI = "sqlite:////tmp/anbado-test.db"
+SOCKET_PORT = 8888
+DEBUG = True
---
+++ anbadoserver/database.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+import anbadoserver.config as Config
+
+
+db_engine = create_engine(Config.DATABASE_URI, echo=Config.DEBUG, convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=db_engine))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
