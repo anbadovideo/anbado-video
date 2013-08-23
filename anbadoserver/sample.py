a3e6c921a5dc4a6974338a3293a2f72cf5f2b4a2
Chiwan Park <chiwanpark@icloud.com>
2013-08-23 11:09:35 +0900

    add sample event send feature into SampleNamespace.

--- anbadoserver/sample.py
+++ anbadoserver/sample.py
@@ -1,19 +1,90 @@
 #!/usr/bin/env python
 
-from gevent import monkey
+from gevent import monkey, sleep
 
 monkey.patch_all()
 
 from socketio import socketio_manage
 from socketio.namespace import BaseNamespace
+from socketio.mixins import RoomsMixin
 
 import os
+import random
 
 
 class SampleNamespace(BaseNamespace):
     def on_sample(self, data):
         print('sample event raised! data: ' + str(data))
         self.emit('return', data)
+
+
+class SampleVideoNamespace(BaseNamespace, RoomsMixin):
+    video = {
+        'provider': 'youtube',
+        'provider_vid': 'aY_iYRlty9I',
+        'timeline_weight': []
+    }
+
+    def make_room_name(self, provider, provider_vid):
+        return provider + provider_vid
+
+    def initialize(self):
+        pass
+
+    def on_require(self, param):
+        import copy
+
+        if len(self.video['timeline_weight']) == 0:
+            for i in range(257):
+                self.video['timeline_weight'].append(random.randint(0, 20))
+
+        data = copy.deepcopy(self.video)
+        data['success'] = True
+        data['transaction_id'] = param['transaction_id']
+
+        self.join(self.make_room_name(self.video['provider'], self.video['provider_vid']))
+        self.emit('require_response', data)
+
+        def send_event():
+            import random
+
+            while True:
+                event = {
+                    'event_id': random.randint(0, 1048576),
+                    'appeared': random.randint(0, 257),
+                    'type': 1,
+                    'content': 'test',
+                    'user_id': random.randint(0, 65535),
+                    'coord': (
+                        random.randint(0, 1000),
+                        random.randint(0, 1000),
+                        random.randint(0, 1000),
+                        random.randint(0, 1000)
+                    ),
+                    'parent_id': -1
+                }
+                event['disappeared'] = random.randint(event['appeared'], 257)
+                self.emit("event_appeared", event)
+                sleep(random.randint(2, 4))
+
+        self.spawn(send_event)
+
+    def on_post_event(self, param):
+        import random
+
+        data = {
+            'transaction_id': param['transaction_id'],
+            'success': True,
+            'event_id': random.randint(1, 65535)
+        }
+
+        self.emit('post_event_response', data)
+
+    def view_complete(self, param):
+        pass
+
+    def recv_disconnect(self):
+        self.disconnect(silent=True)
 
 
 class Application(object):
@@ -40,8 +111,10 @@ class Application(object):
         elif path.endswith('.html'):
             content_type = 'text/html'
         elif path.startswith('socket.io'):
+            path = path.strip('socket.io')
             namespace_def = {
-                '': SampleNamespace
+                '/socket.io/v1/sample': SampleNamespace,
+                '/socket.io/v1/video': SampleVideoNamespace
             }
 
             socketio_manage(environ, namespace_def, self.request)
