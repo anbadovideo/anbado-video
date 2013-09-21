314a739145acca04ae325a93144cd1d871070038
Chiwan Park <chiwanpark@icloud.com>
2013-09-21 23:23:14 +0900

    add frogspan feature (with Han Jin-Soo <jinsu0411@gmail.com>)

---
+++ anbadoserver/frogspawn.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from flask import json
+import redis
+
+
+class Frog(object):
+    def __init__(self, app):
+        self.app = app
+        self.spawns = {
+            'dummy': DummyFrogspawn,
+            'redis': RedisFrogspawn
+        }
+
+    def breed(self, name):
+        name = name.lower()
+
+        if name not in self.spawns:
+            raise SpawnNotFoundError
+
+        return self.spawns[name](self.app)
+
+
+class Frogspawn(object):
+    def put(self, msg):
+        raise NotImplementedError
+
+
+class DummyFrogspawn(Frogspawn):
+    def __init__(self, app):
+        super(DummyFrogspawn, self).__init__()
+
+    def put(self, msg):
+        pass
+
+
+class RedisFrogspawn(Frogspawn):
+    def __init__(self, app):
+        super(RedisFrogspawn, self).__init__()
+
+        host = app.config.get('FROGSPAWN_REDIS_HOST', 'localhost')
+        port = app.config.get('FROGSPAWN_REDIS_PORT', 6379)
+        db = app.config.get('FROGSPAWN_REDIS_DB', 0)
+
+        self.channel = app.config.get('FROGSPAWN_REDIS_CHANNEL', 'frogspawn')
+        self.pool = redis.ConnectionPool(host=host, port=port, db=db)
+        self.redis = redis.Redis(connection_pool=self.pool)
+
+    def put(self, msg):
+        self.redis.publish(self.channel, json.dumps(msg))
+
+
+class SpawnNotFoundError(Exception):
+    pass
