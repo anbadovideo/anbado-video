26172e6827cc4ab022ad5ea83cadccf301c928f6
Chiwan Park <chiwanpark@icloud.com>
2013-11-09 18:03:38 +0900

    Add documentation string to anbadoserver.api.http module

--- anbadoserver/api/http.py
+++ anbadoserver/api/http.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+""":mod:`anbadoserver.api.http` --- Anbado Video RESTful API
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
 from flask import request, abort, jsonify
 
 from sqlalchemy.exc import SQLAlchemyError
@@ -11,6 +14,14 @@ from anbadoserver.decorators import crossdomain
 @app.route('/user/<int:user_id>', methods=('GET', ))
 @crossdomain(origin='*')
 def user_get(user_id):
+    """Get user information by given user id.
+
+        :param user_id: id of user which you want
+        :type user_id: int
+        :resheader Content-Type: application/json
+        :status 404: when no user found using given user id
+        :status 200: when all operations are successful
+    """
     user = User.by_user_id(user_id)
     if user is None:
         abort(404)
@@ -21,6 +32,16 @@ def user_get(user_id):
 @app.route('/user', methods=('POST', ))
 @crossdomain(origin='*')
 def user_post():
+    """Add new user into solution's database.
+
+        :form user_id: optional id of new user
+        :form name: name of new user
+        :form profile_image: profile image data represented as address or base64-encoded string
+        :resheader Content-Type: application/json
+        :status 400: when some parameters is missing
+        :status 500: when error occurred in database operations
+        :status 200: when all operations are successful
+    """
     profile_image = request.form.get('profile_image', None)
     if profile_image is None:
         abort(400)
@@ -41,6 +62,17 @@ def user_post():
 @app.route('/user/<int:user_id>', methods=('PUT', ))
 @crossdomain(origin='*')
 def user_put(user_id):
+    """Modify user information.
+
+        :form profile_image: new profile image data
+        :param user_id: id of user which you want to change
+        :type user_id: int
+        :resheader Content-Type: application/json
+        :status 200: when all operations are successful
+        :status 400: when profile_image is missing
+        :status 404: when no user found using given user id
+        :status 500: when error occurred in database operations
+    """
     profile_image = request.form.get('profile_image', None)
     if profile_image is None:
         abort(400)
@@ -59,6 +91,14 @@ def user_put(user_id):
 @app.route('/video/<int:video_id>', methods=('GET', ))
 @crossdomain(origin='*')
 def video_get(video_id):
+    """Get video information by given video id.
+
+        :param video_id: id of video which you want
+        :type video_id: int
+        :resheader Content-Type: application/json
+        :status 200: when all operations are successful
+        :status 404: when no video found using given id
+    """
     video = Video.by_video_id(video_id)
     if video is None:
         abort(404)
@@ -69,6 +109,19 @@ def video_get(video_id):
 @app.route('/video', methods=('POST', ))
 @crossdomain(origin='*')
 def video_post():
+    """Add a new video into solution's database.
+
+        :form provider: unique string which represents video provider such as youtube, vimeo, ted, anbado
+        :form provider_id: id of video using in video provider
+        :form title: title of video
+        :form length: length of video in seconds
+        :form user_id: id of user who uploads this video
+        :form video_id: optional id of video if video id is predefined
+        :resheader Content-Type: application/json
+        :status 200: when all operations are successful
+        :status 400: when some required parameters are missing or given user_id is not valid
+        :status 500: when error occurred in database operations
+    """
     provider = request.form.get('provider', None)
     provider_vid = request.form.get('provider_vid', None)
     title = request.form.get('title', '')
@@ -100,6 +153,17 @@ def video_post():
 @app.route('/video/<int:video_id>', methods=('PUT', ))
 @crossdomain(origin='*')
 def video_put(video_id):
+    """Modify video information which has a given video id.
+        **This procedure needs to same parameters using in** :http:post:`/video`
+
+        :param video_id: id of video which you want to change information
+        :type video_id: int
+        :resheader Content-Type: application/json
+        :status 200: when all opertions are successful
+        :status 404: when no video found using given id
+        :status 400: when new video id is missing
+        :status 500: when error occurred in database operations
+    """
     video = Video.by_video_id(video_id)
     if video is None:
         abort(404)
@@ -123,6 +187,15 @@ def video_put(video_id):
 @app.route('/video/<int:video_id>/participants', methods=('GET', ))
 @crossdomain(origin='*')
 def participants_get(video_id):
+    """Get list of participants of certain video.
+        This procedure doesn't consider about friendship.
+
+        :param video_id: id of video which you want to get list of participants
+        :type video_id: int
+        :resheader Content-Type: application/json
+        :status 200: when all operations are successful
+        :status 404: when no video found using given id
+    """
     video = Video.by_video_id(video_id)
     if video is None:
         abort(404)
@@ -133,6 +206,13 @@ def participants_get(video_id):
 @app.route('/friendship/<int:user_id>', methods=('GET', ))
 @crossdomain(origin='*')
 def friendship_get(user_id):
+    """Get list of friends of user found by given id.
+
+        :param user_id: id of user who you want to get list of friends
+        :type user_id: int
+        :status 200: when all operations are successful
+        :status 404: when no user found using given id
+    """
     user = User.by_user_id(user_id)
     if user is None:
         abort(404)
@@ -143,6 +223,16 @@ def friendship_get(user_id):
 @app.route('/friendship/<int:userA_id>/<int:userB_id>', methods=('POST', ))
 @crossdomain(origin='*')
 def friendship_post(userA_id, userB_id):
+    """Add friend relationship between user A and user B.
+
+        :param userA_id: id of user A
+        :type userA_id: int
+        :param userB_id: id of user B
+        :type userB_id: int
+        :status 200: when all operations are successful
+        :status 404: no user found by given user id (a or b)
+        :status 500: when error occurred in database operations
+    """
     userA = User.by_user_id(userA_id)
     userB = User.by_user_id(userB_id)
 
@@ -173,6 +263,16 @@ def friendship_post(userA_id, userB_id):
 @app.route('/friendship/<int:userA_id>/<int:userB_id>', methods=('DELETE', ))
 @crossdomain(origin='*')
 def friendship_delete(userA_id, userB_id):
+    """Delete friend relationship between user A and user B.
+
+        :param userA_id: id of user A
+        :type userA_id: int
+        :param userB_id: id of user B
+        :type userB_id: int
+        :status 200: when all operations are successful
+        :status 404: no user found by given user id (a or b)
+        :status 500: when error occurred in database operations
+    """
     userA = User.by_user_id(userA_id)
     userB = User.by_user_id(userB_id)
 
