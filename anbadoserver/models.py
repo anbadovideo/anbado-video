59116c7f0094028d966df6dde61f9bebced69a14
Taylor <taylor@Taylor.local>
2014-01-06 00:04:47 +0900

    Fix DB Text bug content.

--- anbadoserver/models.py
+++ anbadoserver/models.py
@@ -232,15 +232,10 @@ class Event(db.Model, JsonifiedModel):
     @hybrid_property
     def content(self):
         return self._content
-        #return self._content.decode('utf-8')
 
     @content.setter
     def content(self, value):
         self._content = value
-        #if isinstance(value, unicode):
-        #    self._content = value.encode('utf-8')
-        #else:
-        #    self._content = value
 
     @hybrid_property
     def coord(self):
